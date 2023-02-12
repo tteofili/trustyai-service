0b907917b0c54cfc73d5815b05f6734867a69790
Rui Vieira <ruidevieira@googlemail.com>
2023-02-12 00:49:32 +0000

    Update demo, move to sub-folder and add data logger

--- src/main/java/org/kie/trustyai/service/data/readers/MinioReader.java
+++ src/main/java/org/kie/trustyai/service/data/readers/MinioReader.java
@@ -1,8 +1,16 @@
 package org.kie.trustyai.service.data.readers;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import javax.inject.Singleton;
+
 import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
-import io.minio.errors.*;
+import io.minio.errors.MinioException;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 import org.kie.trustyai.explainability.model.Dataframe;
@@ -10,24 +18,12 @@ import org.kie.trustyai.explainability.model.PredictionInput;
 import org.kie.trustyai.explainability.model.PredictionOutput;
 import org.kie.trustyai.service.data.readers.utils.CSVUtils;
 
-import javax.inject.Singleton;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Singleton
 public class MinioReader implements DataReader {
 
     private static final Logger LOG = Logger.getLogger(MinioReader.class);
 
     private final MinioClient minioClient;
-    private final Dataframe df;
 
     private final String bucketName;
 
@@ -35,22 +31,22 @@ public class MinioReader implements DataReader {
     private final String outputFilename;
 
     public MinioReader(@ConfigProperty(name = "MINIO_ENDPOINT") String endpoint,
-                       @ConfigProperty(name = "MINIO_BUCKET_NAME", defaultValue = "inputs") String bucketName,
-                       @ConfigProperty(name = "MINIO_INPUT_FILENAME") String inputFilename,
-                       @ConfigProperty(name = "MINIO_OUTPUT_FILENAME") String outputFilename,
-                       @ConfigProperty(name = "MINIO_ACCESS_KEY") String accessKey,
-                       @ConfigProperty(name = "MINIO_SECRET_KEY") String secretKey) {
+            @ConfigProperty(name = "MINIO_BUCKET_NAME", defaultValue = "inputs") String bucketName,
+            @ConfigProperty(name = "MINIO_INPUT_FILENAME") String inputFilename,
+            @ConfigProperty(name = "MINIO_OUTPUT_FILENAME") String outputFilename,
+            @ConfigProperty(name = "MINIO_ACCESS_KEY") String accessKey,
+            @ConfigProperty(name = "MINIO_SECRET_KEY") String secretKey) {
         LOG.info("Starting MinIO storage consumer");
         this.bucketName = bucketName;
         this.inputFilename = inputFilename;
         this.outputFilename = outputFilename;
-        LOG.info("MinIO data location: endpoint=" + endpoint + ", bucket=" + bucketName + ", input file=" + inputFilename + ", output filename=" + outputFilename);
+        LOG.info("MinIO data location: endpoint=" + endpoint + ", bucket=" + bucketName + ", input file=" + inputFilename
+                + ", output filename=" + outputFilename);
         this.minioClient =
                 MinioClient.builder()
                         .endpoint(endpoint)
                         .credentials(accessKey, secretKey)
                         .build();
-        this.df = parseData();
     }
 
     public Dataframe parseData() {
@@ -79,6 +75,6 @@ public class MinioReader implements DataReader {
 
     @Override
     public Dataframe asDataframe() {
-        return this.df;
+        return parseData();
     }
 }
